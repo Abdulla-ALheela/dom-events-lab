3e21d9ff851472bab80b939c78e01ab8141d62b6
Abdulla Alheela <Abdulla.alheela@gmail.com>
2024-11-13 20:13:30 +0300

    Lav Solution

--- app.js
+++ app.js
@@ -1,40 +1,85 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
-let numberEl 
+let operatorEl = ""
+let fNumberEl = ""
+let sNumberEl = "" 
 let firstNumber = ""
 /*------------------------ Cached Element References ------------------------*/
 
 const buttonsEl = document.querySelectorAll('.button');
+
+const numberButton = document.querySelectorAll(".number")
+
+const resetButton = document.querySelector(".reset")
+
+const equalsButton = document.querySelectorAll(".equals")
+
+const operatorButton = document.querySelectorAll(".operator")
 
 const calculator = document.querySelector('#calculator');
 
 const displayEl = document.querySelector('.display');
 
 /*----------------------------- Event Listeners -----------------------------*/
-buttonsEl.forEach((button) => {
-    button.addEventListener('click', (event) => {
-        
-        numberEl = event.target.innerText
-        displayEl.append(numberEl);
-        
+  
+function secondNumber(event){
+  if(sNumberEl === ""){
+        sNumberEl += event.target.innerText
+        displayEl.append(sNumberEl);
+  }
+}
+  const first = numberButton.forEach((number) => {
+    number.addEventListener('click', (event) => {
+       if(operatorEl === "/" || operatorEl === "*" || operatorEl === "+" || operatorEl === "-"){
+       
+       secondNumber(event)
+       }else if(fNumberEl === ""){
+        fNumberEl += event.target.innerText
+        displayEl.append(fNumberEl);
+       }
     });
   });
+
+
+  operatorButton.forEach((operator) => {
+    operator.addEventListener('click', (event) => {
+       
+      if(operatorEl === ""){
+      operatorEl = event.target.innerText
+        displayEl.append(operatorEl);
+      }
+    });
+  });
+
+    resetButton.addEventListener('click', reset)
+       
+    
+   
+    equalsButton.forEach((equlas) => {
+      equlas.addEventListener('click', () => {
+        
+        if(operatorEl === "+"){
+          
+          displayEl.textContent = Number(fNumberEl) + Number(sNumberEl)
+         }else if(operatorEl === "-") {
+          displayEl.textContent = Number(fNumberEl) - Number(sNumberEl)
+          
+         }else if(operatorEl === "/") {
+          displayEl.textContent = Number(fNumberEl) / Number(sNumberEl)
+         }else if(operatorEl === "*") {
+          displayEl.textContent = Number(fNumberEl) * Number(sNumberEl)
+         }
+
+    });})
+
   
 /*-------------------------------- Functions --------------------------------*/
-// calculator.addEventListener('click', (event) => {
-//     // This log is for testing purposes to verify we're getting the correct value
-//     // You have to click a button to see this log
-    
-  
-//     // Example
-//     if (numberEl.contains('number')) {
-//       // Do something with a number
-//     }
-  
-//     // Example
-//     if (event.target.innerText === '*') {
-//       // Do something with this operator
-//     }
-//   });
-  
+
+const reset = () => {
+
+  displayEl.textContent = ""
+  operatorEl = ""
+  fNumberEl = ""
+  sNumberEl = ""
+}
